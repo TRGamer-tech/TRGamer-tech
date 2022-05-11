fca17e25a05ab22e7acf8b6305e221eb82633fe4
TRGamer-tech <54816030+TRGamer-tech@users.noreply.github.com>
2022-05-11 23:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    $("body").append('<div class="jscheck">REMOTE JS IS WORKING, WHOOOOO</div>');  
+    $("body").append('<div class="jscheck">REMOTE JS IS WORKING, WHOOOOOO</div>');  
 
 });
